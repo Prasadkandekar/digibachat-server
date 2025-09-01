9a237c163f04a4d4cb809eb17d79d2fce4797299
Prasadkandekar <prasadkandekar2@gmail.com>
2025-09-01 20:10:42 +0530

    group crud workkikng

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? 'https://yourdomain.com' 
-    : ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
-  credentials: true
+  origin: "*",
 }));
 
 // Rate limiting
